c1c2be23610795cb0bafeb5e24e67ef4e4388a17
lukagabadze <gabadzeluka2003@gmail.com>
2025-02-06 13:07:17 +0400

    utils fixes

--- topics/trees/AVLTrees/utils.py
+++ topics/trees/AVLTrees/utils.py
@@ -5,7 +5,17 @@ class TreeNode(object):
         self.right = right
 
 
+class AVLTreeNode(object):
+    def __init__(self, val=0, left=None, right=None, height=1):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.height = height
+
 def array_to_node_tree(arr):
+  if not arr:
+    return None
+
   nodes = [TreeNode(val) if val is not None else None for val in arr]
 
   n = len(nodes)
@@ -32,53 +42,56 @@ def array_to_node_tree(arr):
 HUGE Thanks to BcK https://stackoverflow.com/a/65865825/15109015
 """
 def print_tree(root, val="val", left="left", right="right"):
-    def display(root, val=val, left=left, right=right):
-        """Returns list of strings, width, height, and horizontal coordinate of the root."""
-        # No child.
-        if getattr(root, right) is None and getattr(root, left) is None:
-            line = '%s' % getattr(root, val)
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
+  if not root:
+    return
 
-        # Only left child.
-        if getattr(root, right) is None:
-            lines, n, p, x = display(getattr(root, left))
-            s = '%s' % getattr(root, val)
-            u = len(s)
-            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-            shifted_lines = [line + u * ' ' for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+  def display(root, val=val, left=left, right=right):
+    """Returns list of strings, width, height, and horizontal coordinate of the root."""
+    # No child.
+    if getattr(root, right) is None and getattr(root, left) is None:
+      line = '%s' % getattr(root, val)
+      width = len(line)
+      height = 1
+      middle = width // 2
+      return [line], width, height, middle
 
-        # Only right child.
-        if getattr(root, left) is None:
-            lines, n, p, x = display(getattr(root, right))
-            s = '%s' % getattr(root, val)
-            u = len(s)
-            first_line = s + x * '_' + (n - x) * ' '
-            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
-            shifted_lines = [u * ' ' + line for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+    # Only left child.
+    if getattr(root, right) is None:
+      lines, n, p, x = display(getattr(root, left))
+      s = '%s' % getattr(root, val)
+      u = len(s)
+      first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+      second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+      shifted_lines = [line + u * ' ' for line in lines]
+      return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
 
-        # Two children.
-        left, n, p, x = display(getattr(root, left))
-        right, m, q, y = display(getattr(root, right))
-        s = '%s' % getattr(root, val)
-        u = len(s)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
-        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
-        if p < q:
-            left += [n * ' '] * (q - p)
-        elif q < p:
-            right += [m * ' '] * (p - q)
-        zipped_lines = zip(left, right)
-        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
-        return lines, n + m + u, max(p, q) + 2, n + u // 2
+    # Only right child.
+    if getattr(root, left) is None:
+      lines, n, p, x = display(getattr(root, right))
+      s = '%s' % getattr(root, val)
+      u = len(s)
+      first_line = s + x * '_' + (n - x) * ' '
+      second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+      shifted_lines = [u * ' ' + line for line in lines]
+      return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
 
-    lines, *_ = display(root, val, left, right)
-    print()
-    for line in lines:
-        print(line)
-    print()
+    # Two children.
+    left, n, p, x = display(getattr(root, left))
+    right, m, q, y = display(getattr(root, right))
+    s = '%s' % getattr(root, val)
+    u = len(s)
+    first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+    second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+    if p < q:
+        left += [n * ' '] * (q - p)
+    elif q < p:
+        right += [m * ' '] * (p - q)
+    zipped_lines = zip(left, right)
+    lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+    return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+  lines, *_ = display(root, val, left, right)
+  print()
+  for line in lines:
+      print(line)
+  print()
