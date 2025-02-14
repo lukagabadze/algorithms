ed7d8023d5a222fbda5dce4e5b2372a14816e7a6
lukagabadze <gabadzeluka2003@gmail.com>
2025-02-14 15:25:15 +0400

    solved leetcode problem 417 pacific atlantic water flow

--- topics/trees/BFS/417.Pacific_Atlantic_Water_Flow.py
+++ topics/trees/BFS/417.Pacific_Atlantic_Water_Flow.py
@@ -1,64 +1,53 @@
+"""
+Thanks to the GOAT Abhishek for the amazing solution!
+https://leetcode.com/problems/pacific-atlantic-water-flow/solutions/1126938/short-easy-w-explanation-diagrams-simple-graph-traversals-dfs-bfs/
+"""
+
+
 from collections import deque
 
 """
-TIME: 688ms HORRIBLE, but accepted
-TODO: Look for a better solution
+TIME: 80ms
+NOTE: Much better than my previous brute force BFS solution.
 """
 class Solution(object):
   def pacificAtlantic(self, heights):
     m = len(heights)
     n = len(heights[0])
-    answers = []
-
-    # Initialize the ocean map with (-1, -1).
-    # This will hold the (pacific, atlantic) values for
-    # every point which we already know the answer to.
-    ocean_map = [[(-1, -1)] * len(row) for row in heights]
     
-    for i in range(m):
-      for j in range(n):
+    def travel(queue):
+      visited = set()
+      while queue:
+        (x, y) = queue.popleft()
+        visited.add((x, y))
+        
+        points_of_interest = [
+          (x - 1, y),
+          (x + 1, y),
+          (x, y + 1),
+          (x, y - 1),
+        ]
+        filtered = [
+          (k, f) for (k, f) in points_of_interest
+          if (k, f) not in visited
+          and k >= 0 and k < m and f >= 0 and f < n
+          and heights[k][f] >= heights[x][y]
+        ]
 
-        queue = deque([(i, j)])
-        visited = {(i, j)}
-        pacific = False
-        atlantic = False
-        while queue:
-          (x, y) = queue.popleft()
-          
-          pacific = pacific or x == 0 or y == 0
-          atlantic = atlantic or x == m - 1 or y == n - 1
-          
-          if pacific and atlantic:
-            break
-          
-          points_of_interest = [
-            (x - 1, y),
-            (x + 1, y),
-            (x, y + 1),
-            (x, y - 1),
-          ]
-          filtered = [
-            (k, f) for (k, f) in points_of_interest
-            if (k, f) not in visited
-            and k >= 0 and k < m and f >= 0 and f < n
-            and heights[k][f] <= heights[x][y]
-          ]
+        for k, f in filtered:
+          queue.append((k, f))
+      return visited
 
-          for k, f in filtered:
-            visited.add((k, f))
-
-            if ocean_map[k][f][0] != -1:
-              pacific = pacific or ocean_map[k][f][0]
-              atlantic = atlantic or ocean_map[k][f][1]
-            else:
-              queue.append((k, f))
-
-        # If the point reached to both pacific and atlantic update the answers array
-        if pacific and atlantic:
-          answers.append([i, j])
-
-        # Mark the result in ocean_map for later navigation
-        ocean_map[i][j] = (pacific, atlantic)
+    # Pacific edges
+    queue = deque([(i, 0) for i in range(m)] + [(0, i) for i in range(n)])
+    pacific_visited = travel(queue)
+    
+    # Atlantic edges
+    queue = deque([(i, n - 1) for i in range(m)] + [(m - 1, i) for i in range(n)])
+    atlantic_visited = travel(queue)
+    
+    # Answers are points which were covered in poth pacific and atlantic BFS travels
+    answers = [[i, j] for i, j in pacific_visited & atlantic_visited]
 
     return answers
 
@@ -66,9 +55,9 @@ class Solution(object):
 if __name__ == "__main__":
   solution = Solution()
   
-  # heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+  heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
   
-  heights = [[2,1],[1,2]]
+  # heights = [[2,1],[1,2]]
 
   print("heights:");
   for row in heights:
