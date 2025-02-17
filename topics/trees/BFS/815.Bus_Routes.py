355d8ed000289b5f394a3a1019c6ba40c8f59b8f
lukagabadze <gabadzeluka2003@gmail.com>
2025-02-17 11:23:19 +0400

    tiny improvement to 815 bus routes

--- topics/trees/BFS/815.Bus_Routes.py
+++ topics/trees/BFS/815.Bus_Routes.py
@@ -2,10 +2,23 @@ from collections import deque
 
 
 """
+First try:
+TIME: 820ms (Beats 41.96%)
+TODO: This can be improved, look into it
+
+Second try:
 TIME: 91ms (Beats 84.14%)
 NOTE: Much better than my previous method which only kept track of visited lines.
 This time, I have visited_stops and visited_lines sets which help me avoid useless routes.
 Also, I kept line indexes in the queue, now I keep stops.
+
+Third try:
+TIME: 90ms (Beats 84.58%)
+NOTE: This time, I put lines in the queue like in my first method,
+BUT, I kept track of visited_stops alongside my ususal visited_lines
+and the result is the same good performance.
+Looks like the difference between 820ms and 90ms-91ms is just
+keeping track of both visited lines and visited stops in a set.
 """
 
 
@@ -14,6 +27,8 @@ class Solution:
         # Base case
         if source == target:
             return 0
+
+        routes_set = [set(route) for route in routes]
 
         bus_stop_map = {}
         for i, route in enumerate(routes):
@@ -28,26 +43,26 @@ class Solution:
 
         lines = bus_stop_map[source]
 
-        queue = deque([(source, 0)])
+        queue = deque([(line, 1) for line in lines])
+        visited_lines = set(lines)
         visited_stops = set([source])
-        visited_lines = set()
         while queue:
-            (stop, ans) = queue.popleft()
+            (line, ans) = queue.popleft()
 
-            lines = bus_stop_map[stop]
+            if target in routes_set[line]:
+                return ans
 
-            for line in lines:
-                if line in visited_lines:
+            stops = routes[line]
+
+            for stop in stops:
+                if stop in visited_stops:
                     continue
 
-                visited_lines.add(line)
-                stops = routes[line]
-                for stop in stops:
-                    if stop == target:
-                        return ans + 1
-                    if stop not in visited_stops:
-                        queue.append((stop, ans + 1))
-                        visited_stops.add(stop)
+                visited_stops.add(stop)
+                for stop_line in bus_stop_map[stop]:
+                    if stop_line not in visited_lines:
+                        queue.append((stop_line, ans + 1))
+                        visited_lines.add(stop_line)
 
         return -1
 
@@ -55,17 +70,17 @@ class Solution:
 if __name__ == "__main__":
     solution = Solution()
 
-    routes = [[1, 2, 7], [3, 6, 7]]
-    source = 1
-    target = 6
+    # routes = [[1, 2, 7], [3, 6, 7]]
+    # source = 1
+    # target = 6
 
     # routes = [[7, 12], [4, 5, 15], [6], [15, 19], [9, 12, 13]]
     # source = 15
     # target = 12
 
-    # routes = [[2], [2, 8]]
-    # source = 8
-    # target = 2
+    routes = [[2], [2, 8]]
+    source = 8
+    target = 2
 
     print("routes: ", routes)
     print("source: ", source)
