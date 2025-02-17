69bc625d21e3dc35beced5e87ae00baebec2ef3c
lukagabadze <gabadzeluka2003@gmail.com>
2025-02-17 12:33:32 +0400

    significantly improved 127 word ladder

--- topics/trees/BFS/127.Word_Ladder.py
+++ topics/trees/BFS/127.Word_Ladder.py
@@ -1,38 +1,39 @@
 from collections import deque
 from typing import List
+import string
 
 
 """
+Method 1:
 TIME: 8497ms (Beats 5.00%)
 NOTE: Improve the solution this result is just disgusting
+
+Method 2:
+TIME: 334ms (Beats 36.63%)
+NOTE: Much better! In this method, you don't go through the each word in wordList every iteration.
+Instead, you assemble the new word by changing out each character by a new one since `beginWord, endWord, and wordList[i] consist of lowercase English letters`.
+HUGE Thanks to Himanshu Malik for the solution! (https://leetcode.com/problems/word-ladder/solutions/1764371/a-very-highly-detailed-explanation/)
 """
 
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordList = set(wordList)
         queue = deque([(beginWord, 1)])
         visited = set([beginWord])
         while queue:
-            (currentWord, ans) = queue.popleft()
+            (word, ans) = queue.popleft()
 
-            for word in wordList:
-                if word not in visited:
-                    if self.two_words_diff(currentWord, word) == 1:
-                        visited.add(word)
-                        queue.append((word, ans + 1))
-
-                        if word == endWord:
+            for i in range(len(word)):
+                for c in string.ascii_lowercase:
+                    new_word = word[:i] + c + word[i + 1 :]
+                    if new_word not in visited and new_word in wordList:
+                        visited.add(new_word)
+                        queue.append((new_word, ans + 1))
+                        if new_word == endWord:
                             return ans + 1
 
         return 0
-
-    def two_words_diff(self, word1: str, word2: str) -> int:
-        diff = 0
-        for i in range(len(word1)):
-            if word1[i] != word2[i]:
-                diff += 1
-
-        return diff
 
 
 if __name__ == "__main__":
