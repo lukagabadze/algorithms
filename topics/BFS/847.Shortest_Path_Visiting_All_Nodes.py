1bee350c22ac00c762a7cef1615e2f8e1d97b023
lukagabadze <gabadzeluka2003@gmail.com>
2025-02-22 18:20:25 +0400

    finally fucking solved 847 shortest path visiting all nodes

--- topics/BFS/847.Shortest_Path_Visiting_All_Nodes.py
+++ topics/BFS/847.Shortest_Path_Visiting_All_Nodes.py
@@ -19,10 +19,30 @@ going from each node one by one, is kinda like DFS, when this solution requires 
 It would be better to fill the queue with initial notes first and the virst one to visit all nodes wins.
 Thank you to the GOAT Panda for the solution: https://leetcode.com/problems/shortest-path-visiting-all-nodes/solutions/135809/fast-bfs-solution-46ms-clear-detailed-explanation-included/
 Glad I understand this problem now.
+
+NOTE: My way of solving was correct but had 2 major problems:
+1) I had a for loop where I ran BFS from every node only when the last node was finished.
+This was a major flaw since it was no longer BFS, I had to calculate the answer using `answer = min(answer, ans)`.
+This has been fixed now by filling the queue with each node as a starter node before starting the BFS while loop.
+This ensures that the first time you visit all nodes, it is the shortest path!!!
+
+2) Second major mistake was my way of keeping track of visited_nodes.
+I had a set of nodes, which I used to transform into a string like this `1-3-6-7-9`,
+I also had to sort it which added more time, time which was very crucial to this problem.
+This has been fixed by Bitmasks, holy fucking shit bitmasks are cool.
+Bitmasks took my solution into another level in terms of performance.
+Cool skill to have!
+Bit Masking Tutorial: https://www.hackerearth.com/practice/algorithms/dynamic-programming/bit-masking/tutorial/
 """
 
 from collections import deque
 from typing import List
+
+
+"""
+TIME: 111ms (Beats 89.36%)
+Bitmasks are BLAZING FAST WTF?!
+"""
 
 
 class Solution:
@@ -32,47 +52,40 @@ class Solution:
 
         # Number of nodes
         n = len(graph)
+        target = (1 << n) - 1
 
         answer = float("+inf")
         visited = set()
         queue = deque()
+
+        # Fill the queue with every node as a started node.
+        # This ensures that the first time we visit all nodes, it will be the shortest!
         for i in range(len(graph)):
-            queue.append((i, set(), set([i])))
-            visited.add((i, f"{i}"))
+            queue.append((i, 1 << i, 0))
+            visited.add((i, 1 << i))
 
-        found = False
         while queue:
-            (node, visited_paths, visited_nodes) = queue.popleft()
+            (node, visited_nodes_mask, ans) = queue.popleft()
 
-            if found:
+            if visited_nodes_mask == target:
+                answer = min(answer, ans)
                 break
 
             for next_node in graph[node]:
-                if (
-                    next_node,
-                    "-".join(map(str, sorted(visited_nodes))),
-                ) not in visited:
-                    next_node_visited_paths = visited_paths.copy()
-                    next_node_visited_paths.add((node, next_node))
+                # Calculate the next_node_visited_mask using the
+                # visited_nodes_mask with the XOR operation on a
+                # bitmask with only next_node set as 1 (1 << next_node)
+                next_node_visited_mask = visited_nodes_mask | (1 << next_node)
 
-                    next_node_visited_nodes = visited_nodes.copy()
-                    next_node_visited_nodes.add(next_node)
+                if (next_node, next_node_visited_mask) not in visited:
+                    # Found the answer, since it's BFS, return, it's the shortest
+                    if next_node_visited_mask == target:
+                        return ans + 1
 
-                    visited.add((node, "-".join(map(str, sorted(visited_nodes)))))
+                    queue.append((next_node, next_node_visited_mask, ans + 1))
+                    visited.add((next_node, next_node_visited_mask))
 
-                    if len(next_node_visited_nodes) == n:
-                        answer = min(answer, len(next_node_visited_paths))
-                        found = True
-
-                    queue.append(
-                        (
-                            next_node,
-                            next_node_visited_paths,
-                            next_node_visited_nodes,
-                        )
-                    )
-
-        return answer
+        return -1
 
 
 if __name__ == "__main__":
