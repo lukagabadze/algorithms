1cdde146b832149fba306b5f294b16d714e38503
lukagabadze <gabadzeluka2003@gmail.com>
2025-03-01 22:07:40 +0400

    reverting back to dfs solution to keep solutions organized

--- topics/DFS/1372.Longest_ZigZag_Path_in_a_Binary_Tree.py
+++ topics/DFS/1372.Longest_ZigZag_Path_in_a_Binary_Tree.py
@@ -5,6 +5,8 @@ TODO: See what happens when you use BFS on this problem.
 NOTE: I changed the stack to a queue and the result is 3ms faster, now this was only run once so the results are rough estimates,
 so I am concluding that both BFS and DFS perform the same here, they are both O(n) algorithms in this case.
 I am committing this BFS solution but I will commit a DFS solution after it so that my DFS folder holds DFS solutions.
+
+NOTE: I just ran the DFS solution again and it gave back 56ms, before it was 61ms (3ms longer than BFS).
 """
 
 from utils import array_to_node_tree, print_tree
@@ -12,7 +14,7 @@ from collections import deque
 
 
 """
-DFS TIME: 61ms (Beats 63.67%)
+DFS TIME: 56ms (Beats 76.46%)
 I am BLAZING through these problems!
 
 BFS TIME 59ms (Beats 70.42%)
@@ -23,26 +25,26 @@ I thought I finally found a good DFS problem but in this problem it does not mat
 class Solution(object):
     def longestZigZag(self, root):
         queue = deque([(root, None, 0)])
-        visited = set([root])
+        stack = set([root])
         answer = 0
         while queue:
-            (node, direction, depth) = queue.popleft()
+            (node, direction, depth) = queue.pop()
 
             answer = max(answer, depth)
 
-            if node.right and node.right not in visited:
+            if node.right and node.right not in stack:
                 new_depth = depth + 1
                 if direction == "R":
                     new_depth = 1
                 queue.append((node.right, "R", new_depth))
-                visited.add(node.right)
+                stack.add(node.right)
 
-            if node.left and node.left not in visited:
+            if node.left and node.left not in stack:
                 new_depth = depth + 1
                 if direction == "L":
                     new_depth = 1
                 queue.append((node.left, "L", new_depth))
-                visited.add(node.left)
+                stack.add(node.left)
 
         return answer
 
