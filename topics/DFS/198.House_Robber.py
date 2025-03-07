d67a9ecb6aa48c0f62ba29ef0d808d4df9a1a0e7
lukagabadze <gabadzeluka2003@gmail.com>
2025-03-07 23:20:49 +0400

    added notes to leetcode 198 house robber

--- topics/DFS/198.House_Robber.py
+++ topics/DFS/198.House_Robber.py
@@ -4,6 +4,10 @@ Without the @cache it gets TLE.
 
 NOTE: Interesting fact, you can't use @cache if one of the parameters of a function is a list you get an error: unhashable type: 'list'.
 So instead, you can use tuples, it works exactly the same here, calculating the length and splitting the tuple, the syntax is same as arrays.
+
+NOTE: This was my solution, but you can solve this much more simply without the @cache by using dynamic programming.
+Check out niits solution (https://leetcode.com/problems/house-robber/solutions/6147042/2-solutions-o-n-space-and-o-1-space)
+Time complexity is O(n), you can not go shorte than that, but you can optimize memeory by only saving last to values of the dynamic calculations.
 """
 
 from typing import List, Tuple
