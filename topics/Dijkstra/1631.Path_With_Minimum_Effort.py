b2b587b31c931463aa4261ccface5f3730c98dad
lukagabadze <gabadzeluka2003@gmail.com>
2025-03-17 20:23:41 +0400

    solved leetcode 1631 path with minimum effort

---
+++ topics/Dijkstra/1631.Path_With_Minimum_Effort.py
@@ -0,0 +1,73 @@
+"""
+NOTE: My Dijkstra solutions are passing but they are performing worse than the average.
+I don't know why that is, maybe these problems are better suited for Bellman Ford algorithm or something, idk I have not learned it yet.
+"""
+
+from typing import List
+from collections import defaultdict
+from heapq import heappop, heappush
+
+
+"""
+TIME: 734ms (Beats 22.76%)
+NOTE: Can be improved!
+"""
+
+
+class Solution(object):
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m = len(heights)
+        n = len(heights[0])
+
+        # Heap containing (effort, (row, col))
+        heap = [(0, (0, 0))]
+        efforts = defaultdict(int)
+        while heap:
+            (effort, (i, j)) = heappop(heap)
+
+            if (i, j) in efforts:
+                continue
+
+            efforts[(i, j)] = effort
+
+            points_of_interest = [
+                (i + 1, j),
+                (i - 1, j),
+                (i, j + 1),
+                (i, j - 1),
+            ]
+            for x, y in points_of_interest:
+                if x >= 0 and y >= 0 and x < m and y < n:
+                    new_effort = max(
+                        effort,
+                        abs(heights[x][y] - heights[i][j]),
+                    )
+                    heappush(heap, (new_effort, (x, y)))
+
+        return efforts[(m - 1, n - 1)]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    q = [
+        [[1, 2, 2], [3, 8, 2], [5, 3, 5]],
+        [[1, 2, 3], [3, 8, 4], [5, 3, 5]],
+        [
+            [1, 2, 1, 1, 1],
+            [1, 2, 1, 2, 1],
+            [1, 2, 1, 2, 1],
+            [1, 2, 1, 2, 1],
+            [1, 1, 1, 2, 1],
+        ],
+        [[1, 10, 6, 7, 9, 10, 4, 9]],
+    ]
+
+    for heights in q:
+        for row in heights:
+            print(row)
+        print()
+        answer = solution.minimumEffortPath(heights)
+        print("answer: ", answer)
+        print("=====================")
+        print()
