5f9d6ab4b570830ffb522c4680df6be4d206e8c9
lukagabadze <gabadzeluka2003@gmail.com>
2025-03-18 18:13:35 +0400

    solved leetcode 1368 minimum cost to make at least one valid path in a grid (solved in 12 minutes btw 😎🐐)

---
+++ topics/Dijkstra/1368.Minimum_Cost_to_Make_at_Least_One_Valid_Path_in_a_Grid.py
@@ -0,0 +1,50 @@
+"""
+NOTE: DAAAAMNN I Solved this in 12 fucking minutes! I am becoming the fucking GOAT!
+"""
+
+from typing import List
+from heapq import heappop, heappush
+
+
+class Solution(object):
+    def minCost(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        directions = {1: (0, 1), 2: (0, -1), 3: (1, 0), 4: (-1, 0)}
+
+        heap = [(0, (0, 0))]
+        costs = [[float("inf") for _ in range(n)] for _ in range(m)]
+        while heap:
+            cost, (i, j) = heappop(heap)
+
+            if i == m - 1 and j == n - 1:
+                return cost
+
+            for key in directions:
+                di, dj = directions[key]
+                x, y = i + di, j + dj
+
+                if x >= 0 and y >= 0 and x < m and y < n:
+                    new_cost = cost + 1 if key != grid[i][j] else cost
+
+                    if new_cost < costs[x][y]:
+                        heappush(heap, (new_cost, (x, y)))
+                        costs[x][y] = new_cost
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    q = [
+        [[1, 1, 1, 1], [2, 2, 2, 2], [1, 1, 1, 1], [2, 2, 2, 2]],
+        [[1, 1, 3], [3, 2, 2], [1, 1, 4]],
+        [[1, 2], [4, 3]],
+    ]
+
+    for heights in q:
+        for row in heights:
+            print(row)
+        print()
+        answer = solution.minCost(heights)
+        print("answer: ", answer)
+        print("=====================")
+        print()
