3e81f4c1eee2642987fc8164e9cfc31df6ee282d
lukagabadze <gabadzeluka2003@gmail.com>
2025-04-21 18:53:50 +0400

    solved leetcode 684 redundant connection, but it needs to improved performance wise

---
+++ topics/Prim/684.Redundant_Connection.py
@@ -0,0 +1,93 @@
+"""
+NOTE: "1 <= ai < bi <= edges.length" This constraint might be the KEY to returning the last edge in the input in case of multiple answers.
+Because currently I have NO clue how I can find the last edge in the input in O(n) time.
+"""
+
+from typing import List
+from heapq import heappop, heappush
+
+
+"""
+TIME: 11ms (Beats 15.07%)
+MEMORY: 18.14MB (Beats 78.34%)
+NOTE: I don't like this, I need to improve this.
+"""
+
+
+class Solution(object):
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+
+        graph = [[] for _ in range(n + 1)]
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        for i, [a, b] in enumerate(edges[::-1]):
+            heap = [(0, 1, -1)]
+            visited = set()
+            found = True
+            while heap:
+                dist, node, parent = heappop(heap)
+
+                if node in visited:
+                    found = False
+                    break
+
+                visited.add(node)
+
+                for neighbour in graph[node]:
+                    if min(node, neighbour) == a and max(node, neighbour) == b:
+                        continue
+
+                    if neighbour not in visited:
+                        heappush(heap, (dist + 1, neighbour, node))
+
+            if found and len(visited) == n:
+                return [a, b]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    q = [
+        ([[1, 2], [1, 3], [2, 3]]),
+        ([[1, 2], [2, 3], [3, 4], [1, 4], [1, 5]]),
+        (
+            [
+                [9, 10],
+                [5, 8],
+                [2, 6],
+                [1, 5],
+                [3, 8],
+                [4, 9],
+                [8, 10],
+                [4, 10],
+                [6, 8],
+                [7, 9],
+            ]
+        ),
+        (
+            [
+                [2, 7],
+                [7, 8],
+                [3, 6],
+                [2, 5],
+                [6, 8],
+                [4, 8],
+                [2, 8],
+                [1, 8],
+                [7, 10],
+                [3, 9],
+            ]
+        ),
+    ]
+
+    for edges in q:
+        for s, e in edges:
+            print(s, e)
+        print()
+        answer = solution.findRedundantConnection(edges)
+        print("answer: ", answer)
+        print("=====================")
+        print()
