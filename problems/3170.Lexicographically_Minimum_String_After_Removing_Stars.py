d3cc3223922c878a508a4db862437440e76ac547
lukagabadze <gabadzeluka2003@gmail.com>
2025-06-07 15:42:42 +0400

    solved leetcode 3170 lexicographically minimum string after removing stars

---
+++ problems/3170.Lexicographically_Minimum_String_After_Removing_Stars.py
@@ -0,0 +1,43 @@
+from heapq import heappush, heappop
+
+
+"""
+
+"""
+class Solution:
+    def clearStars(self, s: str) -> str:
+        n = len(s)
+
+        heap = []
+        removed_inds = set()
+        for i in range(n):
+            if s[i] != "*":
+                heappush(heap, (s[i], -i))
+            else:
+                c, c_i = heappop(heap)
+                removed_inds.add(-c_i)
+
+        answer = ""
+        for i in range(n):
+            if s[i] != "*" and i not in removed_inds:
+                answer += s[i]
+
+        return answer
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    q = [
+        ("aaba*"),
+        ("abc"),
+        ("abdac*d*cc*"),
+    ]
+
+    for s in q:
+        print("s: ", s)
+        print()
+        answer = solution.clearStars(s)
+        print("answer: ", answer)
+        print("=====================")
+        print()
